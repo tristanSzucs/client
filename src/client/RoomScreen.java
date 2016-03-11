b9505249177460a6378657b6943634463ab5bec6
Tristan <Tristan@Tristan-Think.Home>
2016-03-11 13:14:02 -0500

    Added RoomList and Room Line
    Finished Linked list to Add and Dec Pop along with removeing and adding
    rooms

--- src/client/RoomScreen.java
+++ src/client/RoomScreen.java
@@ -1,26 +1,152 @@
 package client;
 
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class RoomScreen extends JPanel 
+
+
+
+/*
+ * This class contains the following private subclasses
+ * RoomList
+ * RoomLine
+ */
+
+public class RoomScreen extends JFrame 
 {
 	private RoomList roomList = new RoomList();
+	private RoomLine first = null;
 	
 	public RoomScreen() {
+		super("TEST");
+		RoomList test = new RoomList();
+		this.add(test );
+		test.addRoom("TEST", 5);
+		test.addRoom("TEST1", 6);
+		test.addRoom("TEST3", 45);
+		//test.addRoom("TT", 24);
+		test.addRoom("TEST43245", 24352);
+		this.setVisible(true);
+		this.setSize(500,500);
+		test.RemovePopRoom("TEST");
 		
+		test.reDo();
 	}
 	
 	private class RoomList extends JPanel 
 	{
+		
 		//the constructor
 		public RoomList() {
-			
-			
-			
+			this.setLayout(new GridLayout(10,1));
 		}
-	}
+		
+		/*
+		 * Adds a room based on Init pop and name
+		 */
+		public void addRoom(String name, int pop) {
+			//creates the room to add
+			RoomLine toAdd = new RoomLine(name, pop);
+			//adds it to the GUI
+			add(toAdd  );
+			//if the list is empty make it the first
+			if(first == null) {
+				first = toAdd;
+				return;
+			}
+			
+			//otherwise go through and add it to the end
+			RoomLine cur = first;
+			//go to the end
+			while(cur.next != null) cur = cur.next;
+			//add it
+			cur.next = toAdd;
+		}
+		
+		/*
+		 * This method removes the room based on the name
+		 */
+		public void RemoveRoom(String name) {
+			//if there is none then nothing happens
+			if(first == null) {
+				return;
+			}
+			//if it is the first element
+			if(first.getName() == name) {
+				remove(first);	//removes from GUI
+				first = first.next; //removes from list
+				reDo();	//redraws the GUI
+			}
+			
+			//start at top
+			RoomLine cur = first;
+			//while there is more and the next is not the one we need then keep moving
+			while(cur.next != null && cur.next.getName() != name) cur = cur.next;
+			if (cur.next == null) return; //if the case was that there is no more then end
+			//otherwise the next is to be removed
+			remove( cur.next );	//removes the element from GUI
+			reDo();	//redraw
+			cur.next = cur.next.next;	//removes from list
+		} //end of method
+		
+		/*
+		 * THis method takes in a string name and adds a pop to it
+		 */
+		public void AddPopRoom(String name) {
+			//if it is empty then return
+			if(first == null) {
+				return;
+			}
+		
+			//go through the rest of the list
+			RoomLine cur = first;  //set the curent to the first
+			
+			//while we are not at null and this is not the one we are looking for
+			while(cur != null && cur.getName() != name)	cur = cur.next;  
+			//if case was there is no more 
+			if (cur == null) return;
+			cur.addPop();
+		} //end of method
+		
+		
+		/*
+		 * This method removes this line if a room by the name is found
+		 * If not found it is not removed.
+		 */
+		public void RemovePopRoom(String name) {
+			//if it is empty then return
+			if(first == null) {
+				return;
+			}
+		
+			//go through the rest of the list
+			RoomLine cur = first;  //set the curent to the first
+			
+			//while we are not at null and this is not the one we are looking for
+			while(cur != null && cur.getName() != name)	cur = cur.next;  
+			//if case was there is no more 
+			if (cur == null) return;
+			cur.decPop();
+		} //end of method
+		
+		/*
+		 * This function removes all lines and puts them back on.
+		 */
+		public void reDo() {
+			this.removeAll();
+			RoomLine cur = first;
+			while(cur != null) {
+				add(cur);
+				cur = cur.next;
+			}
+		} //end of reDO
+		
+	} //end of RoomList
 	
 	
 	/*
@@ -33,7 +159,8 @@ public class RoomScreen extends JPanel
 		private int pop;		//the population of the room
 		private JLabel label;
 		private JButton button;
-		
+		private ActionListener listener;
+		private RoomLine next = null;
 		
 		/*
 		 * The constructor
@@ -41,17 +168,22 @@ public class RoomScreen extends JPanel
 		 * iName = the name of the room
 		 * number = the amount of people in the room
 		 */
-		public void RoomLine (String iName, int number) {
+		public RoomLine (String iName, int number) {
+			this.setLayout(new GridLayout(1,2));	//set a grid layout
+			
+			//set up the private variables
 			name = iName;
 			pop = number;
 			
+			//make the JLabel
 			label = new JLabel("");
 			update();				//sets the data into the label
 			add(label);
 			
-			button = new JButton("join");
+			//set up the button
+			button = new JButton("JOIN");
+			button.addActionListener(listener);
 			add(button);
-			
 		} //end of constructor
 		
 		
@@ -85,12 +217,20 @@ public class RoomScreen extends JPanel
 		 * This method updates the display of this object
 		 */
 		public void update() {
-			label.setText(String.format("%15s %5d", name, pop));
-		}
+			label.setText(String.format("%-25s %-5d", name, pop));
+			this.validate();
+		} //end of method
 		
 		//returns the name of the room this class is for.
 		public String getName() {
 			return name;
-		}
+		} //end of method
+	} //end of class RoomLine
+	
+	public static void main(String args[]) {
+		new RoomScreen();
+		
 	}
-}
+	
+	
+} //end of class
